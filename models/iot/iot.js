8f2cc10f3ae23a0b3ef76f531cafcfc7a30e58cd
Mint <mskylinee@hotmail.com>
2015-10-30 15:47:51 +0700

    backend.iot

---
+++ models/iot/iot.js
@@ -0,0 +1,11 @@
+var restful = require('node-restful')
+var mongoose = restful.mongoose
+
+var iot = new mongoose.Schema({
+  timestamp: Number,
+  iot_id: Number,
+  temperature: Number,
+  relative_humidity: Number
+})
+
+module.exports = restful.model('iot', iot)
---
+++ route/api.js
@@ -0,0 +1,9 @@
+var express = require('express')
+var router = express.Router()
+
+var iot = require('../models/iot/iot.js')
+
+iot.methods(['get', 'put', 'post', 'delete'])
+iot.register(router, '/iot')
+
+module.exports = router
