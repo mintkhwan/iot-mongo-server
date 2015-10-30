18258ee11397fb4887f90f570e046055c495fa61
Mint <mskylinee@hotmail.com>
2015-10-30 16:15:28 +0700

    frontend.iot

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var bodyParser = require('body-parser')
 var mongoose = require('mongoose')
-
+var Model = require('./models/iot/iot.js')
 mongoose.connect('mongodb://localhost/iot')
 
 var app = express()
@@ -10,6 +10,27 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
 app.use('/api', require('./route/api'))
+
+app.post('/', function (req, res) {
+  var obj = new Model(req.body)
+  obj.save(function (err, obj) {
+    if (err) {
+      res.status(500).send(err)
+    } else {
+      res.send(obj)
+    }
+  })
+})
+
+app.get('/', function (req, res) {
+  Model.find({}).exec(function (err, results) {
+    if (err) {
+      res.status(500).send(err)
+    } else {
+      res.send(results)
+    }
+  })
+})
 
 app.listen(3000)
 console.log('run in 3000')
