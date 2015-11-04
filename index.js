4cb95d4024b1fde99bf5393e90ce829bfed185fc
Mint <mskylinee@hotmail.com>
2015-11-04 17:37:05 +0700

    start login

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
+////////// require npm install //////////
 var express = require('express')
-
 var bodyParser = require('body-parser')
-
 var mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/iot')
 
+////////// require other file follow path //////////
 var Model = require('./models/iot/iot.js')
-
-var iot = require('./models/iot/iot.js')
-
 var login = require('./models/login/login.route.js')
 
+////////// variable function express //////////
 var app = express()
 
+////////// connect mongoDB name "iot" //////////
+mongoose.connect('mongodb://localhost/iot')
+
+//////////  show index in folder public //////////
 app.use(express.static('public'))
 
+////////// send data between back-end and front-end //////////
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
+////////// require from /route/api //////////
 app.use('/api', require('./route/api'))
 
+////////// require api/login //////////
 app.use('/api/login', login)
 
+////////// insert in root //////////
 app.post('/', function (req, res) {
   var obj = new Model(req.body)
   obj.save(function (err, obj) {
@@ -33,7 +38,7 @@ app.post('/', function (req, res) {
   })
 })
 
-app.get('/', function (req, res) {
+/*app.get('/', function (req, res) {
   Model.find({}).exec(function (err, results) {
     if (err) {
       res.status(500).send(err)
@@ -41,11 +46,12 @@ app.get('/', function (req, res) {
       res.send(results)
     }
   })
-})
+})*/
 
+////////// delete in /api/iot //////////
 app.delete('/api/iot/:id', function (req, res ){
-  return Model.findById(req.params.id, function (err, iot) {
-    return Model.remove(function (err) {
+   Model.findById(req.params.id, function (err, iot) {
+   Model.remove(function (err) {
       if (!err) {
         console.log("removed")
           return res.send('')
@@ -56,5 +62,6 @@ app.delete('/api/iot/:id', function (req, res ){
   })
 })
 
+////////// use port 3000 to run node //////////
 app.listen(3000)
-console.log('run in 3000')
+console.log('run in port 3000')
--- public/register.js
+++ public/register.js
@@ -1,9 +1,12 @@
 angular.module('todoApp', [])
   .controller('TodoListController', function($http,$interval) {
     var todoList = this
+
+    getlogin()
+    
     
     todoList.register = function (input) {
-      //alert('success Acount:'+input.name)
+      alert('success Acount:'+input.name)
       saveRegister(input)
     }
 
@@ -12,11 +15,57 @@ angular.module('todoApp', [])
         .then(function success (response) {
           console.log(response)
           alert('Success')
-          //window.location = "thankyou.html"
+          window.location = "thankyou.html"
           
         }, function error (response) {
           alert(response.data.message)
         })
     }
+
+    function getlogin () {
+      console.log('yes')
+      $http.get('/api/login')
+        .then(function success (response) { 
+          todoList.login = response.data
+           
+          console.log(todoList.login)
+          //console.log(response)
+        }, function error (response) {
+          alert(response.data.message)
+        })
+    }
+
+
+
+    todoList.adduser = function  (input) {
+      console.log('yes')
+      $http.get('/api/login')
+        .then(function success (response) { 
+          todoList.login = response.data
+           
+            for(var i =0;i<response.data.length;i++){
+              
+              if(response.data[i].username==input.username && response.data[i].password==input.password){
+                console.log(' success : '+response.data[i].username)
+                break;
+              }
+              else if(response.data[i].username!=input.username && response.data[i].password!=input.password){
+                console.log('err')
+              }
+            }
+
+          console.log(todoList.login)
+          //console.log(response)
+        }, function error (response) {
+          alert(response.data.message)
+        })
+    }
+
+    
+
+
+
+
+
     
   })
